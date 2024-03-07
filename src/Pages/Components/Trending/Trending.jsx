450849e1607dedbe7b3f84e7403b1048b91d8fac
Jashany <jsingh12_be22@thapar.edu>
2024-03-07 22:43:16 +0530

    minor fix - trending

--- src/Pages/Components/Trending/Trending.jsx
+++ src/Pages/Components/Trending/Trending.jsx
@@ -1,13 +1,12 @@
 import styles from './Trending.module.css';
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 const Trending = ({trending}) => {
     
     return ( 
         <div className={styles.maintrend}>
             <h1>Trending Coins (24h)</h1>
             <div className={styles.trending}>
-                {trending.slice(0,3).map(item => <TrendingPost key={item.item?.id} item={item.item} />)}
+                {trending?.slice(0,3).map(item => <TrendingPost key={item.item?.id} item={item.item} />)}
             </div>
         </div>
      );
